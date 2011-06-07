9853c1eed114a898fc9d60c907c65438a79a10fc
Gehan Gonsalkorale <gehan.g@gmail.com>
2011-06-08 00:52:34 +0100

    Add more captains

--- cotd/management/setup_data.py
+++ cotd/management/setup_data.py
@@ -3,4 +3,6 @@ from cotd.models import *
 def setup():
     Captain.objects.create(name="Sam", fbid="502605077")
     Captain.objects.create(name="Taryn", fbid="670923540")
+    Captain.objects.create(name="Gehan", fbid="502531952")
+    Captain.objects.create(name="Tim", fbid="51039793")
     Config.objects.create(access_token="103428196417085|2ec9f49255d67724228d4c3e.1-502531952|mcnFVD514urhHcbxAzyKLusaFS4")
